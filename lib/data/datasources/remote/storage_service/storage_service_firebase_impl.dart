e37fbd5005ecdb9487159f85a186e0487274b6c9
benik <galstyan.benik@gmail.com>
2024-12-01 21:35:58 +0200

    rewrote model to freezed

--- lib/data/datasources/remote/storage_service/storage_service_firebase_impl.dart
+++ lib/data/datasources/remote/storage_service/storage_service_firebase_impl.dart
@@ -1,19 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_master/data/datasources/models/question_model.dart';
+import 'package:flutter_master/data/models/question_model.dart';
 import 'package:flutter_master/data/datasources/storage_service.dart';
 
 class StorageServiceFirebaseImpl implements StorageService {
   final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
   static const _isCorrectField = 'is_correct';
+  static const _questionIdField = 'questionId';
   static const _questionsCollection = 'questions';
 
   @override
   Future<List<QuestionModel>> getQuestions() async {
-    // TODO(Benik): we need try branche here, or we can do it only for domain?
+    // TODO(Benik): we need try branch here, or we can do it only for domain?
     try {
       final snapshot = await _fireStore.collection(_questionsCollection).get();
-      return snapshot.docs.map((doc) => QuestionModel.fromFirestore(doc.data(), doc.id)).toList();
+      return snapshot.docs
+          .map((doc) => QuestionModel.fromJson({
+                _questionIdField: doc.id,
+                ...doc.data(),
+              }))
+          .toList();
     } catch (e) {
       rethrow;
     }
@@ -21,9 +27,10 @@ class StorageServiceFirebaseImpl implements StorageService {
 
   @override
   Future<void> updateQuestionStatus(String questionId, bool isCorrect) async {
-    // TODO(Benik): we need try branche here, or we can do it only for domain?
+    // TODO(Benik): we need try branch here, or we can do it only for domain?
     try {
-      final docRef = _fireStore.collection(_questionsCollection).doc(questionId);
+      final docRef =
+          _fireStore.collection(_questionsCollection).doc(questionId);
       await docRef.update({_isCorrectField: isCorrect});
     } catch (e) {
       rethrow;
